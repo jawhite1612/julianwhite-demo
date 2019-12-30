b36e03686f025b1b68a5144a047433ec2d727727
jawhite1612 <jawhite1612@gmail.com>
2019-12-29 22:11:46 -0500

    testing deployment

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const express = require('express')
 const path = require('path')
+const fs = require('fs');
 const PORT = process.env.PORT || 5000
 
 express()
   .use(express.static(path.join(__dirname, 'public')))
   .set('views', path.join(__dirname, 'views'))
+  .set('images', path.join(__dirname, 'images'))
+  .set('scripts', path.join(__dirname, 'scripts'))
   .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/test'))
+  .get('/', (req, res) => res.render('pages/image_test'))
   .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+
