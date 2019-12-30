6c57c9d92ec0ddbcc68c33326d3f283275e12748
jawhite1612 <jawhite1612@gmail.com>
2019-12-29 20:43:33 -0500

    testing-deployment

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
-const express = require('express')
-const path = require('path')
-const PORT = process.env.PORT || 5000
-
-express()
-  .use(express.static(path.join(__dirname, 'public')))
-  .set('views', path.join(__dirname, 'views'))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('pages/index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+var http = require('http');  
+var url = require('url');  
+var fs = require('fs');  
+var server = http.createServer(function(request, response) {  
+    var path = url.parse(request.url).pathname;  
+    switch (path) {  
+        case '/':  
+            response.writeHead(200, {  
+                'Content-Type': 'text/html'  
+            });  
+            fs.readFile('./test.html', null, function (error, data) {
+                if (error) {
+                    response.writeHead(404);
+                    response.write('Whoops! File not found!');
+                } else {
+                    response.write(data);
+                }
+                response.end();
+            });
+            break;
+        default:  
+            response.writeHead(404);  
+            response.write("opps this doesn't exist - 404");  
+            response.end();  
+            break;  
+    }  
+});  
+server.listen(8082);  
